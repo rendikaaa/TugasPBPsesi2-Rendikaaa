06a0ecc7e28aa02b61b81ab2bb34ddbab1faa4fc
rendikaaa <114813064+rendikaaa@users.noreply.github.com>
2023-10-04 12:37:03 +0700

    Add files via upload

---
+++ TugasSesi2.js
@@ -0,0 +1,8 @@
+const profile = {
+    firstName: "RENDIKA",
+    lastName: "FEBRIAN",
+    age: 20,
+    isMarried : false 
+}
+const {firstName, lastName, age, isMarried} = profile;
+console.log(firstName, lastName, age, isMarried);
